ebcf2eaab39d52fd4605ad93b9b0dab142d2a2c2
matthew238 <matthew.szulik@gmail.com>
2016-12-17 18:27:54 +0900

    [change]テスティング・フレームワークをMinitestからRSpecに変更。

---
+++ spec/campus_code_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe CampusCode do
+  it "has a version number" do
+    expect(CampusCode::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
